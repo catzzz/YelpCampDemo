023c44bf2b3a045b73b0209ebdac988237738dae
Catzzz <chuneleu@gmail.com>
2021-04-30 17:14:10 -0700

    complete basic edit campground

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const Campground = require('./models/campground');
 
@@ -22,7 +23,7 @@ const app = express()
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended: true}));
-
+app.use(methodOverride('_method'));
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -56,6 +57,12 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
 });
+
+app.put('/campgrounds/:id', async(req,res) => {
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
+})
 
 app.listen(3000, () => {
     console.log("LISTEN PORT 3000");
