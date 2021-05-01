db044432497fc5e9cc7b27cc4598df6d1453f123
Catzzz <chuneleu@gmail.com>
2021-04-30 17:00:31 -0700

    create edit page

--- app.js
+++ app.js
@@ -26,33 +26,37 @@ app.use(express.urlencoded({extended: true}));
 
 app.get('/',(req,res)=>{
     res.render('home');
-})
+});
 
 app.get('/campgrounds',async (req,res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index',{campgrounds})
 
-})
+});
 
 app.get('/campgrounds/new', (req, res)=>{
     res.render('campgrounds/new');
-})
+});
 
 app.post('/campgrounds',async(req, res)=>{
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`)
-})
+});
 
 
 app.get('/campgrounds/:id',async(req,res)=>{
     const {id} = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show',{campground})
-})
+});
 
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+});
 
 app.listen(3000, () => {
     console.log("LISTEN PORT 3000");
-})
+});
