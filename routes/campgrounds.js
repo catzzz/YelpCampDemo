bd3c6fa247721aeca176f4fa28232343e3cab416
Catzzz <chuneleu@gmail.com>
2021-05-02 11:46:08 -0700

    setting up flash success

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,14 +57,14 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id',validateCampground , catchAsync(async(req,res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-
+    req.flash('success', 'Successfully update a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.delete('/:id',catchAsync(async (req, res)=>{
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
-    
+    req.flash('success', 'Successfully delete a campground!!!');
     res.redirect('/campgrounds');
 }));
 
