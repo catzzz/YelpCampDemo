77166c9a40717463f29c3a4ed64d0a8349bd10f3
Catzzz <chuneleu@gmail.com>
2021-05-02 10:55:54 -0700

    setup express-session

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
+const session = require('express-session');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -39,10 +41,27 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(morgan('tiny'));
 
+
+
 app.use('/campgrounds',campgrounds);
 app.use('/campgrounds/:id/reviews',reviews);
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge:1000 * 60 * 60 * 24 * 7
+    }
+
+}
+app.use(session(sessionConfig));
 
 app.get('/',(req,res)=>{
     res.render('home');
