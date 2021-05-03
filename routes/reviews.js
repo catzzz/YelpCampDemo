a56c27ac4cfc038b7d2631053903012ea57e94ac
Catzzz <chuneleu@gmail.com>
2021-05-03 16:35:09 -0700

    restructing controller for users and reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,21 +6,13 @@ const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const {reviewSchema } = require('../schemas');
 const Review = require('../models/review');
+const reviews = require('../controllers/reviews');
 const {validateReview,isLoggedIn,isReviewAuthor} = require('../middleware');
 
 
 
 
-router.post('/',isLoggedIn, validateReview,catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Successfully create a new review!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.post('/',isLoggedIn, validateReview,catchAsync(reviews.createAReview))
 
 router.delete('/:reviewId',isLoggedIn,isReviewAuthor,catchAsync(async (req,res)=>{
     const {id, reviewId} = req.params;
--- routes/users.js
+++ routes/users.js
@@ -2,44 +2,22 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 const User = require('../models/user');
+
+const users = require('../controllers/users');
+
+
 const catchAsync = require('../utils/catchAsync');
 
 // Register
-router.get('/register',(req,res)=>{
-    res.render('users/register');
-})
+router.get('/register',users.renderRegister);
 
-router.post('/register',catchAsync(async(req,res)=>{
-    try{
-        const {email, username, password} = req.body;
-        const user = new User({email, username});
-        const registerUser = await User.register(user, password);
-        req.login(registerUser,err =>{
-            if(err){
-                return next(err);
-            }
-            req.flash('success','Welcome to YelCamp');
-            res.redirect('/campgrounds');
-        });
-        
-    }catch(e){
-        req.flash('error',e.message);
-        res.redirect('/register');
-    }
-    
-}));
+router.post('/register',catchAsync(users.registerUser));
 
 // Login
 
-router.get('/login',(req,res)=>{
-    res.render('users/login');
-});
+router.get('/login',users.renderLoginForm);
 
-router.post('/login',passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),(req,res) => {
-    req.flash('success','welcome back');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    res.redirect(redirectUrl);
-});
+router.post('/login',passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),users.login);
 // logout 
 
 router.get('/logout',(req, res)=>{
