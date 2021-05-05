a3497cf0460ca98d07d8b498d6b6a2b3d350c40d
Catzzz <chuneleu@gmail.com>
2021-05-04 20:36:10 -0700

    upodae images in edit, new

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -56,11 +56,20 @@ module.exports.renderEdit = async (req, res) => {
 };
 
 module.exports.updateCampground = async(req,res) => {
-    const {id} = req.params;
-
+    const { id } = req.params;
+    console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    req.flash('success', 'Successfully update a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+    }
+    req.flash('success', 'Successfully updated campground!');
+    res.redirect(`/campgrounds/${campground._id}`)
 };
 
 module.exports.deleteCampground = async (req, res)=>{
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -28,7 +28,7 @@ router.get('/new',isLoggedIn, campgrounds.renderNewForm); // this has to be in f
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn,isAuthor,validateCampground , catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn,isAuthor,upload.array('image'),validateCampground , catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn,isAuthor,catchAsync(campgrounds.deleteCampground));
 
 
