05326751a6d82fc1d1c6ca368fbc8c6b4ba5ca8a
Catzzz <chuneleu@gmail.com>
2021-05-05 17:20:30 -0700

    content security policy

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const reviewRoute = require('./routes/reviews');
 
 const Joi = require('joi');
 const {campgroundSchema ,reviewSchema} = require('./schemas');
+const helmet = require('helmet');
 
 
 const { slice } = require('./seeds/cities');
@@ -65,6 +66,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 
 const sessionConfig = {
+    name:'session',
     secret: 'thisshouldbeabettersecret!',
     resave:false,
     saveUninitialized:true,
@@ -77,6 +79,51 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dxspz43sd/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 
 // Passport setting
